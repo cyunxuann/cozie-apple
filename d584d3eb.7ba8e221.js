16982903698084ee8d260469723623e2c95ddd05
Federico Tartarini <federicotartarini@gmail.com>
2020-07-29 14:25:04 +0800

    Deploy website - based on 29e36ff455e86a5cd673d3d8d4859f61760ffa80

---
+++ d584d3eb.7ba8e221.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{121:function(o){o.exports=JSON.parse('{"allTagsPath":"/cozie-apple/blog/tags","slug":"thermal-comfort","name":"thermal-comfort","count":1,"permalink":"/cozie-apple/blog/tags/thermal-comfort"}')}}]);
